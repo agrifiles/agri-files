5c2c86cdb2f513e16f02312aaede0b6d76483d22
agrifiles <connect.agrifiles@gmail.com>
2025-12-06 14:01:50 +0530

    bug fix - SMTP

--- frontend/src/app/verify-otp/page.js
+++ frontend/src/app/verify-otp/page.js
@@ -3,7 +3,7 @@
 import { useState, useContext } from 'react';
 import { useSearchParams, useRouter } from 'next/navigation';
 import { LangContext } from '../layout';
-import {API_BASE} from '../lib/utils'
+import {API_BASE} from '@/lib/utils'
 
 
 export default function VerifyOtpPage() {
